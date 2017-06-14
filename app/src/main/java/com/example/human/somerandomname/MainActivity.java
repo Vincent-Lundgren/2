048801630360240b3d3021a937bd8974d9334aa8
Vincent <vincentl.job@gmail.com>
2017-06-14 13:48:43 -0600

    cc

--- app/src/main/java/com/example/human/somerandomname/MainActivity.java
+++ app/src/main/java/com/example/human/somerandomname/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.human.somerandomname;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+cc
 public class MainActivity extends AppCompatActivity {
 
   @Override
